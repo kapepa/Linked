dccb9bfd0a66741014becee1aa0c7860921d3159
kapepa <kapepayx@gmail.com>
2022-09-27 19:48:25 +0300

    NewPublicationsComponent

--- app/src/app/component/new-publications/new-publications.component.ts
+++ app/src/app/component/new-publications/new-publications.component.ts
@@ -1,29 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {  AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { PopoverController } from "@ionic/angular";
 import { CreatePublicationComponent } from "../create-publication/create-publication.component";
+import {BehaviorSubject} from "rxjs";
 
 @Component({
   selector: 'app-new-publications',
   templateUrl: './new-publications.component.html',
   styleUrls: ['./new-publications.component.scss'],
 })
-export class NewPublicationsComponent implements OnInit {
+export class NewPublicationsComponent implements OnInit, AfterViewInit {
+  @ViewChild('post') post: ElementRef;
 
   constructor(public popoverController: PopoverController) { }
 
   ngOnInit() {}
 
+  ngAfterViewInit() {
+    this.post['el'].click()
+  }
+
+  closePublication(){
+    console.log('close')
+  }
+
   async onPublication(e: Event){
     const popover = await this.popoverController.create({
       component: CreatePublicationComponent,
-      event: e,
       size: "cover",
-      side: "bottom",
       animated: false,
-      dismissOnSelect: false,
+      componentProps: { onClosePublication: () => popover.dismiss(), },
       cssClass: 'new-publications__create',
     });
 
     return await popover.present();
   }
+
 }
