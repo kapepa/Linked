b76dae70b78010f9552d6bf60ab484fc2d73735d
kapepa <kapepayx@gmail.com>
2022-09-29 12:02:46 +0300

    infinite-scroll fix

---
+++ client/src/app/component/tape-post/tape-post.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tape-post',
+  templateUrl: './tape-post.component.html',
+  styleUrls: ['./tape-post.component.scss']
+})
+export class TapePostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
