0e6a7822d83de02265cbf25264822b8162cc6b3a
kapepa <kapepayx@gmail.com>
2022-10-25 00:59:56 +0300

    unit test FeetService

--- server/src/feet/feet.service.ts
+++ server/src/feet/feet.service.ts
@@ -10,37 +10,37 @@ import {FeetInterface} from "./feet.interface";
 export class FeetService {
   constructor(
     @InjectRepository(Feet)
-    private usersRepository: Repository<Feet>
+    private feetRepository: Repository<Feet>
   ) {}
 
   createFeet(feet: FeetDto): Observable<FeetInterface> {
-    return from(this.usersRepository.save(feet)).pipe(
+    return from(this.feetRepository.save(feet)).pipe(
       catchError(err => { throw err })
     );
   }
 
   getFeet(id: string): Observable<FeetInterface> {
-    return from(this.usersRepository.findOne({ where: { id } })).pipe(
+    return from(this.feetRepository.findOne({ where: { id } })).pipe(
       catchError(err => { throw new HttpException('db didn\'t find those feet.', HttpStatus.NOT_FOUND) })
     )
   }
 
   allFeet(options: { where?: {[key: string]: string | {[key: string]: string }}, take?: number, skip?: number, relations?: string[] }): Observable<FeetInterface[]> {
-    return from(this.usersRepository.find({...options, order: { createdAt: 'DESC' }, relations: ['author']})).pipe(
+    return from(this.feetRepository.find({...options, order: { createdAt: 'DESC' }, relations: ['author']})).pipe(
       catchError(err => { throw new HttpException('db didn\'t not found feet.', HttpStatus.NOT_FOUND)})
     )
   }
 
   // updateFeet(id: string, feet: FeetDto): Observable<UpdateResult> {
   updateFeet(id: string, feet: FeetDto): Observable<FeetInterface> {
-    return from(this.usersRepository.update({ id }, feet )).pipe(
+    return from(this.feetRepository.update({ id }, feet )).pipe(
       switchMap(() => this.getFeet(id)),
-      catchError(err => { throw new HttpException('Something went wrong.', HttpStatus.BAD_REQUEST)})
+      catchError(err => { throw new HttpException('Something went wrong when update feet.', HttpStatus.BAD_REQUEST)})
     )
   }
 
   deleteFeet(id: string): Observable<DeleteResult> {
-    return  from(this.usersRepository.delete({ id })).pipe(
+    return  from(this.feetRepository.delete({ id })).pipe(
       catchError(err => { throw new HttpException('Something went wrong when delete feet.', HttpStatus.NOT_FOUND)})
     )
   }
