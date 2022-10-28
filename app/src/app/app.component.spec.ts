cba5b9ce6f86c9f1a880a528fd19872dc9d4ef7a
kapepa <kapepayx@gmail.com>
2022-10-28 19:48:02 +0300

    unit test PersonComponent

--- app/src/app/app.component.spec.ts
+++ app/src/app/app.component.spec.ts
@@ -1,15 +1,21 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TestBed, waitForAsync } from '@angular/core/testing';
-
 import { AppComponent } from './app.component';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { IonicStorageModule } from "@ionic/storage-angular";
 
 describe('AppComponent', () => {
-
   beforeEach(waitForAsync(() => {
 
     TestBed.configureTestingModule({
-      declarations: [AppComponent],
+      declarations: [
+        AppComponent,
+      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [
+        HttpClientTestingModule,
+        IonicStorageModule.forRoot()
+      ]
     }).compileComponents();
   }));
 
--- app/src/app/modules/person/person.component.spec.ts
+++ app/src/app/modules/person/person.component.spec.ts
@@ -2,15 +2,30 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { PersonComponent } from './person.component';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {HttpClientModule} from "@angular/common/http";
+import {AuthService} from "../../core/service/auth.service";
+import {RouterTestingModule} from "@angular/router/testing";
 
 describe('PersonComponent', () => {
   let component: PersonComponent;
   let fixture: ComponentFixture<PersonComponent>;
 
+  let mockAuthService = {
+
+  }
+
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
+      providers: [
+        { provide: AuthService, useValue: mockAuthService }
+      ],
       declarations: [ PersonComponent ],
-      imports: [IonicModule.forRoot()]
+      imports: [
+        IonicModule.forRoot(),
+        HttpClientTestingModule,
+        RouterTestingModule,
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(PersonComponent);
