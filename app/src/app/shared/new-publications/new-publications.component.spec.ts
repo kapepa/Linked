1ec7ae8cc6350e2701321db4b83cba2a7a6454e6
kapepa <kapepayx@gmail.com>
2022-10-30 18:41:54 +0200

    unit test shared component one part

--- app/src/app/shared/new-publications/new-publications.component.spec.ts
+++ app/src/app/shared/new-publications/new-publications.component.spec.ts
@@ -1,16 +1,35 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
+import {IonicModule, PopoverController} from '@ionic/angular';
 
 import { NewPublicationsComponent } from './new-publications.component';
+import {AuthService} from "../../core/service/auth.service";
+import {of} from "rxjs";
+import {UserClass} from "../../../utils/user-class";
+import {PipeModule} from "../../core/pipe/pipe.module";
 
 describe('NewPublicationsComponent', () => {
   let component: NewPublicationsComponent;
   let fixture: ComponentFixture<NewPublicationsComponent>;
+  let mockUser = UserClass;
+
+  let mockAuthService = {
+    userAvatar: of(mockUser),
+  }
+  let mockPopoverController = {
+    create: () => ({ present: () => {} }),
+  }
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
+      providers: [
+        { provide: AuthService, useValue: mockAuthService },
+        { provide: PopoverController, useValue: mockPopoverController },
+      ],
       declarations: [ NewPublicationsComponent ],
-      imports: [IonicModule.forRoot()]
+      imports: [
+        PipeModule,
+        IonicModule.forRoot(),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(NewPublicationsComponent);
@@ -21,4 +40,13 @@ describe('NewPublicationsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('create new post popup', () => {
+    spyOn(mockPopoverController, 'create');
+    let popup = fixture.debugElement.nativeElement.querySelector('ion-button.new-publications__btn');
+
+    popup.click();
+    expect(mockPopoverController.create).toHaveBeenCalled();
+  })
+
 });
