40c7621e54abad8dac8249a349a9129ff2058672
kapepa <kapepayx@gmail.com>
2023-01-17 00:38:37 +0200

    event client service, event service service

--- server/src/event/event.controller.ts
+++ server/src/event/event.controller.ts
@@ -1,9 +1,11 @@
-import {Controller, Post, Req, UploadedFile, UseGuards, UseInterceptors} from '@nestjs/common';
+import {Body, Controller, Post, Req, UploadedFile, UseGuards, UseInterceptors} from '@nestjs/common';
 import {ApiBody, ApiConsumes, ApiForbiddenResponse, ApiTags} from "@nestjs/swagger";
 import {JwtAuthGuard} from "../auth/jwt-auth.guard";
 import {FileInterceptor} from "@nestjs/platform-express";
 import {multerOption} from "../file/file.service";
 import {Observable, of} from "rxjs";
+import {EventInterface} from "./event.interface";
+import {EventDto} from "./event.dto";
 
 @ApiTags('event')
 @Controller('event')
@@ -15,7 +17,7 @@ export class EventController {
   @ApiBody({description: 'Create new event'})
   @ApiForbiddenResponse({ description: 'Forbidden.'})
   @UseInterceptors(FileInterceptor('img', multerOption ))
-  createEvent(@UploadedFile() img: Express.Multer.File, @Req() req): Observable<any> {
-    return of({});
+  createEvent(@UploadedFile() img: Express.Multer.File, @Req() req, @Body() body: EventDto): Observable<EventInterface> {
+    return of({} as EventInterface);
   }
 }
