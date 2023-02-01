ea8bffd0e4e9359f2f2d5608e5d040aba163d442
kapepa <kapepayx@gmail.com>
2023-02-02 00:00:49 +0200

    app: chat media

--- app/src/app/modules/chat/chat.component.ts
+++ app/src/app/modules/chat/chat.component.ts
@@ -1,12 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {ChatService} from "../../core/service/chat.service";
+import {ChatInterface} from "../../core/interface/chat.interface";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss'],
 })
-export class ChatComponent implements OnInit {
-  constructor() {}
+export class ChatComponent implements OnInit, OnDestroy{
+  view: 'friend' | 'chat' = 'friend';
 
-  ngOnInit() {}
+  chat: ChatInterface;
+  chatSub: Subscription;
+
+  constructor(
+    private chatService: ChatService,
+  ) { }
+
+  ngOnInit() {
+    this.chatSub = this.chatService.getChat.subscribe((chat: ChatInterface) => {
+      this.chat = chat;
+      this.view = 'chat';
+    });
+  }
+
+  ngOnDestroy() {
+    this.chatSub.unsubscribe();
+  }
+
+  onBack(e: Event) {
+    this.view = 'chat';
+  }
+
+  get getView () {
+    return this.view;
+  }
 }
