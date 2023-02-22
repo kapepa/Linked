50e412619e4222becfcfc2b591c34f19934e0379
kapepa <kapepayx@gmail.com>
2023-02-22 23:05:36 +0200

    app: unit-test

--- app/src/app/modules/auth/auth.component.ts
+++ app/src/app/modules/auth/auth.component.ts
@@ -14,7 +14,7 @@ export class AuthComponent implements OnInit {
     private router: Router,
   ) {
     router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) this.url$.next(event.url)
+      if (event instanceof NavigationEnd) this.url$.next(event.url);
     });
   }
 
