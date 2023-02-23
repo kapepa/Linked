e8244ea4d99cfcedebc220c202006b8784893eeb
kapepa <kapepayx@gmail.com>
2023-02-23 20:40:34 +0200

    rebase

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -32,14 +32,11 @@ import { PopupsModule } from "./core/layer/popups/popups.module";
 import { BoardEventsModule } from "./shared/board-events/board-events.module";
 import { PopupPostModule } from "./shared/popup-post/popup-post.module";
 import { GalleryModule } from "./shared/gallery/gallery.module";
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SocialModule } from "./shared/social/social.module";
-
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    NgbModule,
     PipeModule,
     RadioModule,
     SocialModule,
