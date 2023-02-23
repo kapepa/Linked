5e1c90d735d2dff6d79ef5d0b4f7d4a854717403
kapepa <kapepayx@gmail.com>
2023-02-23 23:06:50 +0200

    app: merge web

--- app/src/environments/environment.ts
+++ app/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   configUrl: 'http://127.0.0.1:5000',
   wsUrl: 'ws://localhost:8080',
-  recaptcha: {siteKey: '6LfW4nUkAAAAAEue6_TJwq7fw73FLald0MgTGZ8K'},
+  recaptcha: {siteKey: '6LfW4ncUkAAAAAEue6_TJwq7fw73FLald0MgTGZ8K'},
 };
 
 // 6LfW4nUkAAAAANz57Mznplqqri8528OuC62dKU4p
