e22692f4b2d14dfe53468fabcc00bdff9957fa15
kapepa <kapepayx@gmail.com>
2023-03-05 20:51:03 +0200

    app: unit-test AsideEventComponent

--- app/src/app/shared/aside-event/aside-event.component.spec.ts
+++ app/src/app/shared/aside-event/aside-event.component.spec.ts
@@ -1,16 +1,26 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
+import {IonicModule} from '@ionic/angular';
 
-import { AsideEventComponent } from './aside-event.component';
+import {AsideEventComponent} from './aside-event.component';
+import {RouterTestingModule} from "@angular/router/testing";
+import {EventClass} from "../../../utils/event-class";
+import {EventInterface} from "../../core/interface/event.interface";
 
 describe('AsideEventComponent', () => {
   let component: AsideEventComponent;
   let fixture: ComponentFixture<AsideEventComponent>;
 
+  let classEvent = EventClass as EventInterface;
+
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ AsideEventComponent ],
-      imports: [IonicModule.forRoot()]
+      declarations: [
+        AsideEventComponent,
+      ],
+      imports: [
+        RouterTestingModule,
+        IonicModule.forRoot(),
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(AsideEventComponent);
@@ -21,4 +31,14 @@ describe('AsideEventComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('eventWill', () => {
+
+    let mockDate = new Date(Date.now() + 1000 * 60 * 60);
+    let mockTime = '10:00';
+    let eventWill = component.eventWill(mockDate, mockTime);
+
+    expect(eventWill).toEqual('today in 10:00')
+  })
+
 });
