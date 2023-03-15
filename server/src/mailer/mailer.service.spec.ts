089b0dadb063f13e0b0a1fe533f6e6ebdac3e121
kapepa <kapepayx@gmail.com>
2023-03-15 20:36:56 +0200

    service: unit-test MailerService

--- server/src/mailer/mailer.service.spec.ts
+++ server/src/mailer/mailer.service.spec.ts
@@ -1,18 +1,45 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { MailService } from './mailer.service';
+import {Test, TestingModule} from '@nestjs/testing';
+import {MailService} from './mailer.service';
+import {MailerService} from "@nestjs-modules/mailer";
+import {UserClass} from "../core/utility/user.class";
+import {UsersDto} from "../users/users.dto";
+import {of} from "rxjs";
+
+let MockMailerService = {
+  sendMail: jest.fn(),
+}
 
 describe('MailerService', () => {
   let service: MailService;
+  let mailerService: MailerService;
+
+  let userClass = UserClass;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MailService],
+      providers: [
+        MailService,
+        { provide: MailerService, useValue: MockMailerService },
+      ],
     }).compile();
 
     service = module.get<MailService>(MailService);
+    mailerService = module.get<MailerService>(MailerService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('regUser', () => {
+    let mockUserClass = userClass as UsersDto;
+    let spySendMail = jest.spyOn(mailerService, 'sendMail').mockResolvedValue( () => of({} as any) );
+
+    service.regUser(mockUserClass).subscribe({
+      next: () => {
+        expect(spySendMail).toHaveBeenCalled();
+      }
+    })
+  })
+
 });
