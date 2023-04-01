aea838b997a8d9629005e2e83109953585980b2e
kapepa <kapepayx@gmail.com>
2023-04-02 00:24:32 +0300

    server: unit-test feet:e2e fix

---
+++ server/src/core/utility/comment.class.ts
@@ -0,0 +1,12 @@
+import {UsersDto} from "../../users/users.dto";
+import {FeetDto} from "../../feet/feet.dto";
+
+let CommentClass = {
+  id: 'commentID',
+  host: {} as UsersDto,
+  feet: {} as FeetDto,
+  comment: 'comment',
+  // created_at: new Date(Date.now()),
+}
+
+export {CommentClass}
