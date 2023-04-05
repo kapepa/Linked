08947d205df0f9de7c377a2d229dedf8f156ead2
kapepa <kapepayx@gmail.com>
2023-04-05 15:38:42 +0300

    server: unit-test FeetService

--- server/src/core/utility/feet.class.ts
+++ server/src/core/utility/feet.class.ts
@@ -1,11 +1,18 @@
 import { UsersInterface } from "../../users/users.interface";
 import { FeetInterface } from "../../feet/feet.interface";
 import {CommentInterface} from "../../feet/comment.interface";
+import {AccessEnum} from "../../feet/access.enum";
 
 export let FeetClass = {
   id: 'feetID',
+  img: ['feetImg'],
+  video: '',
+  file: '',
   body: 'body feet',
+  access: AccessEnum.CONTACT,
   author: {} as UsersInterface,
+  like: [],
   comments: [] as CommentInterface[],
+  like_count: 0
   // createdAt: new Date(Date.now()),
 } as FeetInterface;
